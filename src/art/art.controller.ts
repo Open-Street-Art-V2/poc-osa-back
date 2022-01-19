f3b7453f08a5efd0b2bd8b210dc91307ae90ea48
manon <manon.guillard@univ-rouen.fr>
2022-01-19 15:13:23 +0100

    Renommage des routes

--- src/art/art.controller.ts
+++ src/art/art.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Query,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { Art } from './art.entity';
 import { ArtService } from './art.service';
 import { CreateArtDto } from './dto/create-art.dto';
 import { UpdateArtDto } from './dto/update-art.dto';
+import { GetArtsQuery } from './types/query-params.type';
 
 @Controller('art')
 @ApiTags('Art')
@@ -12,35 +23,34 @@ export class ArtController {
   constructor(private readonly artService: ArtService) {}
 
   @UseGuards(JwtAuthGuard)
-  @Post('create')
-  create(@Body() createArtDto: CreateArtDto) :object {
+  @Post()
+  create(@Body() createArtDto: CreateArtDto): Promise<Art> {
     return this.artService.createArt(createArtDto);
   }
 
-  @Get('all')
-  public async getArts() : Promise<Art[]> {
-    return await this.artService.getArts();
+  @Get()
+  public async getArts(@Query() queryParams: GetArtsQuery): Promise<Art[] | Art> {
+    if (Object.keys(queryParams).length === 0) {
+      // if no params in the query
+      return await this.artService.getArts();
+    }
+
+    return await this.artService.getArtByTitle(queryParams.title);
   }
 
   @Get('/:artId')
-  public async getArt(@Param('artId') artId: number) {
+  public async getArt(@Param('artId') artId: number): Promise<Art> {
     return await this.artService.getArt(artId);
   }
 
-  @Get('title/:title')
-  public async getArtByTitle(@Param('title') title: string) {
-    return await this.artService.getArtByTitle(title);
+  @UseGuards(JwtAuthGuard)
+  @Patch('/:artId')
+  update(@Param('artId') artId: number, @Body() updateArtDto: UpdateArtDto): Promise<Art> {
+    return this.artService.editArt(artId, updateArtDto);
   }
 
   @UseGuards(JwtAuthGuard)
-  @Patch('/edit/:artId')
-  update(@Param('artId') artId: number, @Body() updateArtDto: UpdateArtDto) : Promise<Art> {
-
-    return this.artService.editArt(artId,updateArtDto)
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Delete('/delete/:artId')
+  @Delete('/:artId')
   remove(@Param('artId') artId: number) {
     return this.artService.deleteArt(artId);
   }
